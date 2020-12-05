11116253df59c0ad78b5eb428c16d8b94844bdc3
alcns <alican.sariboga.acs@gmail.com>
2020-12-05 21:13:34 +0000

    Fixed

--- src/App.java
+++ src/App.java
@@ -1,10 +1,19 @@
 public class App {
     public static void main(String[] args) {
-       OurLinkedList<String> linkedList = new OurLinkedList<>();
-       linkedList.add("Ali");
-       linkedList.add("Can");
-       linkedList.add("Sifa");
-       linkedList.display();
-
+        OurLinkedList<String> linkedList = new OurLinkedList<>();
+        linkedList.add("Ali");
+        linkedList.add("Can");
+        linkedList.add("Sifa");
+        linkedList.display();
+        System.out.println("===========");
+        linkedList.delete();
+        System.out.println("Linked List Deleted...");
+        System.out.println("===========");
+        linkedList.display();
+        System.out.println("===========");
+        linkedList.delete();
+        System.out.println("Linked List Deleted...");
+        System.out.println("===========");
+        linkedList.display();
     }
 }
